1c94eb58b59931b83e0a259a86b46df20b666c74
sumudusan <ss.sumuduaberathne@gmail.com>
2025-01-12 10:28:32 +0400

    added cors

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -3,21 +3,25 @@ import Order from "../models/order.js";
 // Create a new order
 export async function createOrder(req, res) {
     // Ensure only customers can create orders
-    if (req.user?.type !== "customer") {
-        return res.status(403).json({
+    if (!isCustomer) {
+        res.json({
             message: "Please login as customer to create orders"
-        });
+        }); 
     }
 
     try {
         // Fetch the latest order to generate a new orderId
-        const latestOrder = await Order.find().sort({ date: -1 }).limit(1);
+        // get the id of latest order what we created
+        const   latestOrder = await Order.find().sort({ date: -1 }).limit(1);
 
         let orderId;
+        //chaeck there are not orders yet.
         if (latestOrder.length === 0) {
+            // if the first order (there are not orders yet)should begin with 'CBC0001'.
             orderId = "CBC0001";
         } else {
             const currentOrderId = latestOrder[0]?.orderId || "CBC0000";
+            // get only the number from 'CBC####' and remove 'CBC' from 'CBC####' 
             const numberString = currentOrderId.replace("CBC", "");
             const number = parseInt(numberString, 10);
             const newNumber = (number + 1).toString().padStart(4, "0");
@@ -26,15 +30,44 @@ export async function createOrder(req, res) {
 
         // Prepare order data
         const newOrderData = req.body;
+
+        const newProductArray = []
+
+        for(let i=0; i<newOrderData.orderedItems.length;i++){
+            const product =await Product.findOne({
+                productId : newOrderData.orderedItems[i].productId
+            })
+
+            if(product== null){
+                res.json({
+                    message : "Product with id "
+                    +newOrderData.orderedItems[i].
+                    productId+" not found"
+                })
+                return
+            }
+             newProductArray[i] = {
+            name : product.productName,
+            price : product.price,
+            quantity : newOrderData.orderedItems
+            [i].quantity,
+            Image : product.Images[0]
+           }
+        }
+
+       console.log(newProductArray)
+
+       newOrderData.orderedItems= newProductArray
+
         newOrderData.orderId = orderId;
         newOrderData.email = req.user.email;
 
         // Save order to database
-        const newOrder = new Order(newOrderData);
-        await newOrder.save();
+        const order = new Order(newOrderData);
+        await order.save();
 
         res.status(201).json({
-            message: "Order created successfully"
+            message: "Order created"
          
         });
     } catch (error) {
--- index.js
+++ index.js
@@ -6,11 +6,14 @@ import productRouter from './routes/productRouter.js';
 import userRouter from './routes/userRouter.js';
 import dotenv from "dotenv";
 import orderRouter from './routes/orderRouter.js';
+import cors from "cors";
 dotenv.config(); //load the .env file on your repository
 
 const app = express();
 
 const mongoUrl =process.env.MONGO_DB_URI
+
+app.use(cors())
 
 mongoose.connect(mongoUrl, {}); 
 
